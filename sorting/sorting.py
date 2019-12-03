0aa586d4d94b90cffd710cc3b60a5b0dbe0d65d6
James Casey <hanavi@gmail.com>
2019-12-03 13:40:01 -0600

    Update sorting.py

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -4,7 +4,13 @@
 Filename: search.py
 Author: James Casey
 Date Created: 2019-12-02
-Last Updated: 2019-12-02
+Last Updated: 2019-12-03
+
+This script is designed to visualize various sorting algorithms.  The problem
+turns out not to be implementing the algorithm for the most part, but in
+implementing it in such a way as it can be visualized.  There are probably
+better ways to visualize it than using pygame as well, but I am just doing this
+for fun and to play a little with pygame, so it is what it is!
 """
 
 import click
@@ -41,6 +47,12 @@ MAX_Y = int(HEIGHT / SQUARE_SIZE) - 1
 
 
 def quicksort(data, lo, hi):
+    """ Quick Sort.
+
+    In order to visualize the sorting, we have to do some weird stuff with
+    yeilding....
+    """
+
     if lo < hi:
         p = partition(data, lo, hi)
 
@@ -65,6 +77,7 @@ def quicksort(data, lo, hi):
     yield False
 
 def partition(data, lo, hi):
+    """ Partitioning for Quick Sort.  """
     pivot = data[hi]
     i = lo
     for j in range(lo, hi):
@@ -77,6 +90,11 @@ def partition(data, lo, hi):
 
 
 def insertion_sort(data):
+    """ Insertion Sort.
+
+    In order to visualize the sorting, we have to do some weird stuff with
+    yeilding....
+    """
 
     i = 0
     while i < len(data):
@@ -88,6 +106,7 @@ def insertion_sort(data):
         i += 1
 
     yield False
+
 
 @click.command()
 @click.option("-v", "--verbose", is_flag=True, default=False,
@@ -105,26 +124,29 @@ def main(verbose, draw_grid, insertion, quick):
     else:
         logging.basicConfig(level=logging.WARNING)
 
+    # initialization/setup
     pygame.init()
     pygame.font.init()
-    # myfont = pygame.font.SysFont('couriernewbold', 32)
 
     gDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
     clock = pygame.time.Clock()
 
-    sort_i = 0
+    # Generate a list of data and randomize it
     data = list(range(1,MAX_X))
     random.shuffle(data)
 
-    sorting = False
-    running = True
+
+    # set the sort function
     if quick is True:
         corou = quicksort(data, 0, len(data)-1)
     else:
         corou = insertion_sort(data)
 
+    sorting = False
+    running = True
     while running:
 
+        # handle input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -140,6 +162,8 @@ def main(verbose, draw_grid, insertion, quick):
                         corou = insertion_sort(data)
 
         gDisplay.fill(BLACK)
+
+        # Draw the grid
         if draw_grid is True:
 
             for i in range(1,int(WIDTH/SQUARE_SIZE)):
@@ -150,24 +174,20 @@ def main(verbose, draw_grid, insertion, quick):
                 y = SQUARE_SIZE*i
                 pygame.draw.line(gDisplay, WHITE, (0,y), (WIDTH,y),1)
 
+        # Don't run unless we've clicked to run
         if sorting is True:
             sorting = corou.__next__()
 
-
+        # Draw the data
         for i, value in enumerate(data):
             for j in range(1,value):
                 x = i*SQUARE_SIZE + 1
                 y = (MAX_Y - j)*SQUARE_SIZE + 1
-                pygame.draw.rect(gDisplay, CYAN, pygame.Rect(x,y,SQUARE_SIZE - 1,
-                    SQUARE_SIZE - 1))
-
-
-
+                pygame.draw.rect(gDisplay, CYAN,
+                        pygame.Rect(x,y,SQUARE_SIZE - 1, SQUARE_SIZE - 1))
 
         pygame.display.update()
         clock.tick(30)
 
 if __name__ == "__main__":
     main()
-
-
